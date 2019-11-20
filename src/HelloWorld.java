8f128c2920af7a5fbf25db1eb556a85214b0d7ba
jwhester <jameshester88@gmail.com>
2019-11-20 15:11:11 -0600

    finished

---
+++ src/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Goodbye, JavaScript!");
+        System.out.println("Hello, Java!");
+
+        int myFavoriteNumber = 64;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hello World!";
+        System.out.println(myString);
+
+        float myNumber= (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+////        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        int x = 10;
+//        int y = 2;
+//        x/=y;
+//        y-=x;
+
+    }
+}
